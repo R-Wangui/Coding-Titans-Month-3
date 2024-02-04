41d40fe52e618fccaa60d1b554d0689ce54c28ce
Ching Keziah <chingkeziah@gmail.com>
2024-02-04 04:03:23 -0500

    perf: :zap:

--- categories.js
+++ categories.js
@@ -1,37 +1,50 @@
-document.addEventListener('click' , ()=> {
-    const More = document.getElementById('more')
-    let currPage = 1
-
-    async function moreProducts(page){
-        const res = await fetch('https://fakestoreapi.com/products/category/jewelery?&page=${page}')
-        const products = await res.json()
-        return products
-
-    }
-
-    function displayProducts(products) {
-        const container = document.getElementById('category')
-        products.forEach (product => {
-            const productDiv = createElement('div')
-            productDiv.className = 'product-card'
-            productDiv.innerHTML = `
-            <img src=${product.image} alt=${product.title}>
-            <h3 id="prd-title">${product.title}</h3>
-            <p id="prd-price">$ ${product.price}</p>
-            <p id="prd-category">${product.category}</p>
-            <p id="prd-description">${product.description}</p>
-            <p id="prd-rating">${product.rating.rate} (${product.rating.count} reviews)</p>
-            `
-            container.appendChild(productDiv)
-        })
-    }
-
-    More.addEventListener('click', async() => {
-        const products = await getProducts(currPage)
-        displayProducts(products)
-        currPage++
-    })
-    
-    moreProduct();
-    
-})
+// ? Gets one product from fakestoreapi.com
+async function getOneProduct() {
+    const data = await fetch("https://fakestoreapi.com/products/category/electronics/1");
+    const res = await data.json();
+    return res;
+  }
+  
+  // ? Gets many products from fakestoreapi.com
+  async function getProducts(amount) {
+    const data = await fetch(`https://fakestoreapi.com/products/category/electronics?limit=${amount}`);
+    const res = await data.json();
+  
+    return res;
+  }
+  
+  function displayProducts(products) {
+    const productContainer = document.getElementsByClassName("product-container");
+    products.forEach((product) => {
+      let listItemElement = document.createElement("li");
+      listItemElement.innerHTML = `
+      <div class="product">
+      <img src="${product.image}" alt="">
+      <h3 class="title">${product.title}</h3>
+      <div class="category">
+          <span>${product.category}</span>
+      </div>
+      <div class="description">
+          ${product.description}
+      </div>
+      <div class="price">
+         ${product.price} USD
+      </div>
+  </div>
+  
+      `;
+      productContainer[0].appendChild(listItemElement);
+    });
+  }
+  
+  let counter = 0;
+  let products = await getProducts(3);
+  displayProducts(products);
+  
+  const button = document.getElementsByClassName("btn");
+  button[0].addEventListener("click", async () => {
+    counter += 3;
+    const data = await getProducts(products.length + counter);
+    products = data.slice(counter);
+    displayProducts(products);
+  });
